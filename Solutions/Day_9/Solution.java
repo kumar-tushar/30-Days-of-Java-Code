2797f93d505511a7c327de90726484b2aa19e133
kumar-tushar <kumartushar0110@gmail.com>
2021-05-06 18:45:15 +0530

    6:45 PM

---
+++ Solutions/Day_9/Solution.java
@@ -0,0 +1,19 @@
+package Solutions.Day_9;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static int factorial(int n){
+        if(n<=1)
+            return 1;
+        else
+            return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(factorial(N));
+    }
+}
