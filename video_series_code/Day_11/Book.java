def714836a981df0b3e4044813faf231ce0f16ae
kumar-tushar <kumartushar0110@gmail.com>
2021-05-07 07:37:52 +0530

    7:37 AM

---
+++ video_series_code/Day_11/Book.java
@@ -0,0 +1,8 @@
+package video_series_code.Day_11;
+
+public class Book {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ video_series_code/Day_11/LibraryCatalogue.java
@@ -0,0 +1,8 @@
+package video_series_code.Day_11;
+
+public class LibraryCatalogue {
+
+    public static void main(String[] args) {
+
+    }
+}
