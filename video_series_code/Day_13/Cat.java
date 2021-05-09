b3f191fe901cfce6b68073162d6801328c22d488
kumar-tushar <kumartushar0110@gmail.com>
2021-05-10 02:19:04 +0530

    10 May

---
+++ video_series_code/Day_13/Cat.java
@@ -0,0 +1,18 @@
+package video_series_code.Day_13;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        super(7);
+        System.out.println("A cat has been created");
+    }
+
+    public void meow() {
+        System.out.println("A cat meows!");
+    }
+
+    public void prance() {
+        System.out.println("A cat is prancing");
+    }
+
+}
